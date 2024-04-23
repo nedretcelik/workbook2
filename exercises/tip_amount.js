00b37e48b5f0f2b703b2bc5341fb2e2f9715d967
nedretcelik <nedretcelik97@gmail.com>
2024-04-23 15:47:07 -0400

    add calculation for tip amount of a food bill

---
+++ exercises/tip_amount.js
@@ -0,0 +1,8 @@
+var foodCost = 80;
+var tip =  15
+
+var tipAmount = (foodCost * 15) / 100;
+var foodBill = 80 + tipAmount;
+
+
+console.log("The tip on a " + tipAmount +  " food bill is " + foodBill );
