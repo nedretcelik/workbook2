09bd9f48bbcf6c83e5dcbf01d70e67e4d87917d4
nedretcelik <nedretcelik97@gmail.com>
2024-04-23 15:35:21 -0400

    add exercises

---
+++ demos/decleration.js
@@ -0,0 +1,6 @@
+var firstName = "Nedret";
+var city = "Uskudar";
+var state = "Istanbul"
+var country = "Turkiye"
+
+console.log("Hi! My name is " + firstName + ", I born in " + state + "," + country + " and I grew in " + city + ".");
---
+++ exercises/net_worth.js
@@ -0,0 +1,7 @@
+var assets = 100000;
+var debt = 20000;
+var net_worth;
+
+net_worth = assets - debt;
+
+console.log("Hey! Your net worth is : " + net_worth);
