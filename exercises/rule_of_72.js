f47ddf17a1329d0468007a27a753fd9448fc7e9b
nedretcelik <nedretcelik97@gmail.com>
2024-04-23 17:28:43 -0400

    Add calculation for rule of 72

---
+++ exercises/rule_of_72.js
@@ -0,0 +1,9 @@
+var interestRate = 7;
+var investmentMoney = 1000;
+
+var years = 72 / interestRate;
+
+var doubled = investmentMoney * 2;
+
+console.log("At a " + interestRate + "% interest rate, your savings account will be worth " + doubled + " in " + years.toFixed() + " years")
+
