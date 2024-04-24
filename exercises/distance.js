8455c9261e28f5c0b4963e786bb3ad676d1ec4bd
nedretcelik <nedretcelik97@gmail.com>
2024-04-24 13:27:18 -0400

    Calculation for taxes and distance

---
+++ exercises/distance.js
@@ -0,0 +1,9 @@
+var x1 = 3;
+var x2 = 7;
+var y1 = 2;
+var y2 = 8;
+
+var distance = Math.sqrt(((x2-x1) * (x2-x1)) + ((y2-y1) * (y2-y1)));
+
+
+console.log(distance);
---
+++ exercises/taxes.js
@@ -0,0 +1,10 @@
+var salary = 10000;
+
+var tax = 23;
+
+var taxAmount = (salary * tax ) / 100
+
+var amountOfMoney = salary - taxAmount;
+
+
+console.log("You need to pay every month " + taxAmount + " amount of tax. " + " Your total salary is: " + amountOfMoney);
