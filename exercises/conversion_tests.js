9a14dd2ba06ccd51efeee81d33f054f822550fa8
nedretcelik <nedretcelik97@gmail.com>
2024-04-24 15:54:15 -0400

    add conversion exercises

---
+++ exercises/conversion_tests.js
@@ -0,0 +1,17 @@
+var a = " 101.1 "; 
+var b = "55";
+var c = "402 Stevens";
+var d = "Number 5 ";
+
+// result = doSomething(input);
+aResult = parseInt(a);
+console.log(aResult);
+
+bResult = parseInt(b);
+console.log(bResult);
+
+cResult = parseInt(c);
+console.log(cResult);
+
+dResult = parseInt(d);
+console.log(dResult);
